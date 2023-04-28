498cf8d3edf2e37c58a59019dc6c043fc924eaa9
mokkano0814 <mate.molnar@windowslive.com>
2023-04-28 18:04:05 +0200

    new_battleship_GUI_main

--- Battleship_GUI.py
+++ Battleship_GUI.py
@@ -1,21 +1,89 @@
-import tkinter as tk
-import pygame.mixer
+from tkinter import *
+import pygame
+from PIL import ImageTk, Image
+
+
+
+
 
 def game_setup():
-    start_root = tk.Tk()
+    def show_rules():
+        pass
+        starting_game_button.forget()
+        rules_game_button.forget()
+        exit_game_button.forget()
+
+    def close_rules():
+        pass
+
+    #pygame.mixer.init()
+    #pygame.mixer.music.load('bs_menu_music.mp3')
+    #pygame.mixer.music.play(-1)
+    #pygame.mixer.music.set_volume(0.1)
+    start_root = Tk()
     start_root.geometry("1920x1080")
     start_root.title("Battleship")
-    # root.wm_iconbitmap
-    start_root.resizable(width=False, height=False)
-    game_frame = tk.LabelFrame(start_root, text="Game Board", font=('Myriad Pro', 9), width=650, height=650)
-    game_frame.grid(row=0, column=0, padx=30, pady=50)
-    game_frame.pack_propagate(False)
-    info_frame = tk.LabelFrame(start_root, text="Information / Prepare status", font=('Myriad Pro', 9), width=600,
-                               height=600)
-    info_frame.grid(row=0, column=1, padx=30, pady=50)
-    info_frame.pack_propagate(False)
+    start_root.wm_iconbitmap("battleship_icon_171292.ico")
+
+    menu_frame = Frame(start_root, width=1920, height=1000)
+    menu_frame.pack()
+    menu_frame.pack_propagate(False)
+
+    bg = PhotoImage(file="bs_bg.png")
+    bg_label = Label(menu_frame, image=bg)
+    bg_label.pack(fill=BOTH, expand=True)
+
+    button_width = 10
+    button_height = 3
+    button_padding = 5
+
+    button_style = {
+        'font': ('Myriad Pro', 16, 'bold'),
+        'bg': 'white',
+        'fg': 'black',
+        'bd': 0,
+        'relief': 'ridge',
+        'activebackground': '#CCCCCC',
+        'activeforeground': 'black',
+        'highlightthickness': 0,
+        'highlightbackground': 'white',
+        'highlightcolor': 'white',
+        'padx': 10,
+        'pady': 10,
+        'width': button_width,
+        'height': button_height,
+    }
+    exit_game_button = Button(bg_label, text="Exit Game", **button_style)
+    exit_game_button.pack(side=RIGHT, anchor=SE, padx=button_padding, pady=button_padding)
+
+    rules_game_button = Button(bg_label, text="Rules", **button_style)
+    rules_game_button.pack(side=RIGHT, anchor=SE, padx=button_padding, pady=button_padding)
+    Battl
+    starting_game_button = Button(bg_label, text="Start Game", **button_style)
+    starting_game_button.pack(side=RIGHT, anchor=SE, padx=button_padding, pady=button_padding)
+
+
+
     start_root.mainloop()
-    info_frame_menu = tk.Menu(info_frame
+
+
+
+
+
+
+
+
+
+
+
+
+def exit_game():
+    pass
+
+def starting_game():
+    pass
+
+
 
 
 
